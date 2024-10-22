a560fb0300ece0ee293f040feac664d63c5e7ff7
LyovkinIlya <lyovkin2001@mail.ru>
2024-10-22 14:38:46 +0300

    22.10

--- module_4_1.py
+++ module_4_1.py
@@ -5,7 +5,7 @@ result1 = f_d(5, 2)
 result2 = f_d(1, 0)
 result3 = t_d(10, 2)
 result4 = t_d(2, 0)
-print(result1)
-print(result2)
-print(result3)
-print(result4)
+print(result1) # 2.5
+print(result2) # Ошибка
+print(result3) # 5.0
+print(result4) # inf
